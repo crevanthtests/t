0c7375c417e000326676236f2b81306f3fd6bd1b
crevanth <130873118+crevanth@users.noreply.github.com>
2023-06-03 18:47:27 +0530

    Update and rename script.py to crawlability_check.py

---
+++ crawlability_check.py
@@ -0,0 +1,96 @@
+import requests
+from bs4 import BeautifulSoup
+
+def is_website_crawlable(url):
+    try:
+        # Send an HTTP GET request
+        response = requests.get(url)
+        
+        # Check response code
+        if response.status_code != 200:
+            return False
+
+        # Check meta robots tag
+        if not is_meta_robots_allowed(response):
+            return False
+        
+        # Check for CAPTCHA or other anti-bot mechanisms
+        if has_captcha(response):
+            return False
+        
+        # Check for JavaScript redirection
+        if has_js_redirect(response):
+            return False
+        
+        # Check for infinite loops
+        if has_infinite_loop(response):
+            return False
+        
+        # Check for "nofollow" links
+        if has_nofollow_links(response):
+            return False
+        
+        # Check for dynamic content or AJAX requests
+        if has_dynamic_content(response):
+            return False
+        
+        # Check for login or authentication forms
+        if has_login_form(response):
+            return False
+        
+        # Additional checks...
+        # Add any other checks you want to perform
+        
+        # If all checks passed, the website is crawlable
+        return True
+    
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return False
+
+def is_meta_robots_allowed(response):
+    soup = BeautifulSoup(response.text, "html.parser")
+    meta_robots = soup.find("meta", attrs={"name": "robots"})
+    if meta_robots and meta_robots.get("content"):
+        return "noindex" not in meta_robots["content"].lower()
+    else:
+        # If there is no meta robots tag, assume it's allowed
+        return True
+
+def has_captcha(response):
+    # Check if the response contains a CAPTCHA form or challenge
+    # Implement your own logic to detect CAPTCHA presence
+    return False
+
+def has_js_redirect(response):
+    # Check if there is JavaScript-based redirection
+    # Implement your own logic to detect JavaScript-based redirection
+    return False
+
+def has_infinite_loop(response):
+    # Check if the website has an infinite loop or redirection loop
+    # Implement your own logic to detect infinite loops
+    return False
+
+def has_nofollow_links(response):
+    soup = BeautifulSoup(response.text, "html.parser")
+    nofollow_links = soup.find_all("a", attrs={"rel": "nofollow"})
+    return len(nofollow_links) > 0
+
+def has_dynamic_content(response):
+    # Check if the website uses AJAX requests or has dynamically loaded content
+    # Implement your own logic to detect dynamic content
+    return False
+
+def has_login_form(response):
+    # Check if the website has a login or authentication form
+    # Implement your own logic to detect login forms
+    return False
+
+# Example usage
+website_url = "https://www.amazon.com"
+crawlable = is_website_crawlable(website_url)
+if crawlable:
+    print(f"The website {website_url} is crawlable.")
+else:
+    print(f"The website {website_url} is not crawlable.")
